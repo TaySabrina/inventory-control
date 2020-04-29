c4a1506164978db284b9f9ed04cd1984134eb23d
TaySabrina <taysabrina.prog@gmail.com>
2020-04-28 21:10:03 -0300

    inventory inicial ideas

---
+++ rules.py
@@ -0,0 +1,37 @@
+'''
+How to do??
+
+lists?
+dictionaries?
+functions?
+
+
+lists to items:
+first list: cleaning material
+ - chlorine
+ - detergent
+ - loofah
+ - wax
+ - toilet paper
+ - paper towel
+ - garbage bag
+ - etc..
+
+second list: office supplies
+ - durex
+ - masking tape
+ - sticky notes
+ - file box
+ - scissors
+ - clips
+ - stapler
+ - sulfite sheet
+
+actions:
+ - Can add items
+ - Can delete items
+ - Can put quantity
+ - Can subtract items
+ - warning about less than desired quantity
+ - etc
+'''
